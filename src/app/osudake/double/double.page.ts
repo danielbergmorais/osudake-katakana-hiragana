57522f5bca8a48972ef939728c7371f01a6acf12
Daniel Berg <danielbergmorais@gmail.com>
2026-01-08 16:20:13 -0300

    :construction: Ajustado os grids para osudake

--- src/app/osudake/double/double.page.ts
+++ src/app/osudake/double/double.page.ts
@@ -33,6 +33,12 @@ export class DoublePage implements OnInit {
   selectedBlue: string[] = [];
   cards: Card[] = [];
 
+  cards1: Card[] = [];
+  cards2: Card[] = [];
+  cards3: Card[] = [];
+  cards4: Card[] = [];
+  cards5: Card[] = [];
+
   type$ = this.typeState.type$;
 
   shuffleRed: string[] = [];
@@ -78,24 +84,16 @@ export class DoublePage implements OnInit {
 
   level1Grid: GridItem[] = [
     { char: 'う', color: 'red', row: 2, col: 1 },
-    { char: 'あ', color: 'red', row: 3, col: 2 },
     { char: 'え', color: 'red', row: 2, col: 3 },
+    { char: 'あ', color: 'red', row: 3, col: 2 },
     { char: 'お', color: 'red', row: 4, col: 1 },
-    { char: 'い', color: 'red', row: 4, col: 3 },
+    { char: 'い', color: 'red', row: 7, col: 2 },
 
-
-    // { char: 'う', color: 'red', row: 2, col: 1 },
-    // { char: 'け', color: 'blue', row: 4, col: 1 },
-    // { char: 'く', color: 'blue', row: 6, col: 1 },
-
-    // { char: 'こ', color: 'blue', row: 1, col: 2 },
-    // { char: 'あ', color: 'red', row: 3, col: 2 },
-    // { char: 'き', color: 'blue', row: 5, col: 2 },
-
-    // { char: 'え', color: 'red', row: 2, col: 3 },
-    // { char: 'か', color: 'blue', row: 4, col: 3 },
-    // { char: 'い', color: 'red', row: 6, col: 3 },
-    // { char: 'お', color: 'red', row: 7, col: 2 },
+    { char: 'こ', color: 'blue', row: 1, col: 2 },
+    { char: 'け', color: 'blue', row: 6, col: 3 },
+    { char: 'か', color: 'blue', row: 4, col: 3 },
+    { char: 'く', color: 'blue', row: 6, col: 1 },
+    { char: 'き', color: 'blue', row: 5, col: 2 },
   ];
 
   level2Grid: GridItem[] = [
@@ -104,60 +102,58 @@ export class DoublePage implements OnInit {
     { char: 'か', color: 'blue', row: 5, col: 1 },
     { char: 'こ', color: 'blue', row: 6, col: 2 },
     { char: 'き', color: 'blue', row: 2, col: 2 },
-    { char: 'く', color: 'blue', row: 4, col: 3 },
+    { char: 'く', color: 'blue', row: 3, col: 3 },
 
-    // { char: 'う', color: 'red', row: 1, col: 1 },
-    // { char: 'け', color: 'blue', row: 3, col: 1 },
-    // { char: 'か', color: 'blue', row: 5, col: 1 },
-    // { char: 'あ', color: 'red', row: 7, col: 1 },
+    { char: 'う', color: 'red', row: 1, col: 1 },
+    { char: 'あ', color: 'red', row: 7, col: 1 },
+    { char: 'え', color: 'red', row: 4, col: 2 },
+    { char: 'い', color: 'red', row: 1, col: 3 },
+    { char: 'お', color: 'red', row: 5, col: 3 },
 
-    // { char: 'き', color: 'blue', row: 2, col: 2 },
-    // { char: 'え', color: 'red', row: 4, col: 2 },
-    // { char: 'こ', color: 'blue', row: 6, col: 2 },
-
-    // { char: 'い', color: 'red', row: 1, col: 3 },
-    // { char: 'く', color: 'blue', row: 4, col: 3 },
-    // { char: 'お', color: 'red', row: 7, col: 3 },
   ];
 
   level3Grid: GridItem[] = [
 
-    { char: 'う', color: 'red', row: 3, col: 1 },
+    { char: 'う', color: 'red', row: 1, col: 1 },
     { char: 'お', color: 'red', row: 2, col: 2 },
     { char: 'い', color: 'red', row: 4, col: 2 },
     { char: 'あ', color: 'red', row: 5, col: 3 },
     { char: 'え', color: 'red', row: 3, col: 3 },
 
-
-
-    // { char: 'こ', color: 'blue', row: 2, col: 1 },
-    // { char: 'う', color: 'red', row: 4, col: 1 },
-    // { char: 'け', color: 'blue', row: 6, col: 1 },
-
-    // { char: 'お', color: 'red', row: 1, col: 2 },
-    // { char: 'き', color: 'blue', row: 3, col: 2 },
-    // { char: 'え', color: 'red', row: 5, col: 2 },
-    // { char: 'か', color: 'blue', row: 7, col: 2 },
-
-    // { char: 'い', color: 'red', row: 1, col: 3 },
-    // { char: 'く', color: 'blue', row: 4, col: 3 },
-    // { char: 'あ', color: 'red', row: 6, col: 3 },
+    { char: 'こ', color: 'blue', row: 3, col: 1 },
+    { char: 'け', color: 'blue', row: 6, col: 1 },
+    { char: 'か', color: 'blue', row: 6, col: 2 },
+    { char: 'き', color: 'blue', row: 1, col: 3 },
+    { char: 'く', color: 'blue', row: 7, col: 3 },
   ];
 
   level4Grid: GridItem[] = [
 
     { char: 'こ', color: 'blue', row: 1, col: 1 },
-    { char: 'う', color: 'red', row: 3, col: 1 },
-    { char: 'け', color: 'blue', row: 5, col: 1 },
+    { char: 'け', color: 'blue', row: 6, col: 1 },
+    { char: 'か', color: 'blue', row: 1, col: 2 },
+    { char: 'き', color: 'blue', row: 7, col: 2 },
+    { char: 'く', color: 'blue', row: 2, col: 3 },
 
-    { char: 'お', color: 'red', row: 2, col: 2 },
-    { char: 'き', color: 'blue', row: 4, col: 2 },
-    { char: 'え', color: 'red', row: 6, col: 2 },
-
-    { char: 'か', color: 'blue', row: 1, col: 3 },
-    { char: 'い', color: 'red', row: 3, col: 3 },
-    { char: 'く', color: 'blue', row: 5, col: 3 },
+    { char: 'う', color: 'red', row: 4, col: 1 },
+    { char: 'お', color: 'red', row: 3, col: 2 },
+    { char: 'え', color: 'red', row: 5, col: 2 },
+    { char: 'い', color: 'red', row: 4, col: 3 },
     { char: 'あ', color: 'red', row: 7, col: 3 },
+  ];
+
+  level5Grid: GridItem[] = [
+    { char: 'い', color: 'red', row: 3, col: 1 },
+    { char: 'う', color: 'red', row: 7, col: 3 },
+    { char: 'お', color: 'red', row: 1, col: 2 },
+    { char: 'え', color: 'red', row: 6, col: 2 },
+    { char: 'あ', color: 'red', row: 7, col: 1 },
+
+    { char: 'き', color: 'blue', row: 4, col: 2 },
+    { char: 'こ', color: 'blue', row: 2, col: 3 },
+    { char: 'け', color: 'blue', row: 5, col: 3 },
+    { char: 'か', color: 'blue', row: 1, col: 1 },
+    { char: 'く', color: 'blue', row: 5, col: 1 },
   ];
 
   levels = {
@@ -175,7 +171,7 @@ export class DoublePage implements OnInit {
 
   ngOnInit() {
     this.loadOptions();
-    this.prepareRedGrid();
+    this.prepareGrid();
   }
 
   loadOptions() {
@@ -198,14 +194,63 @@ export class DoublePage implements OnInit {
 
     const shuffled = [...this.selectedRed].sort(() => Math.random() - 0.5);
 
-    this.simpleGrid1 = this.simpleGrid1
+    this.simpleGrid5 = this.simpleGrid5
       .slice(0, shuffled.length)
       .map((item, index) => ({
         ...item,
         char: shuffled[index]
       }));
 
-    this.cards = this.generateLayout(this.simpleGrid1, 7, 3);
+    this.cards = this.generateLayout(this.simpleGrid5, 7, 3);
+  }
+
+  prepareGrid() {
+    console.log(this.selectedRed, this.selectedBlue)
+    if (!this.selectedRed.length) return;
+
+    const shuffled = [...this.selectedRed, ...this.selectedBlue].sort(() => Math.random() - 0.5);
+
+    this.level1Grid = this.level1Grid
+      .slice(0, shuffled.length)
+      .map((item, index) => ({
+        ...item,
+        char: shuffled[index]
+      }));
+
+    this.level2Grid = this.level2Grid
+      .slice(0, shuffled.length)
+      .map((item, index) => ({
+        ...item,
+        char: shuffled[index]
+      }));
+
+    this.level3Grid = this.level3Grid
+      .slice(0, shuffled.length)
+      .map((item, index) => ({
+        ...item,
+        char: shuffled[index]
+      }));
+
+    this.level4Grid = this.level4Grid
+      .slice(0, shuffled.length)
+      .map((item, index) => ({
+        ...item,
+        char: shuffled[index]
+      }));
+
+
+    this.level5Grid = this.level5Grid
+      .slice(0, shuffled.length)
+      .map((item, index) => ({
+        ...item,
+        char: shuffled[index]
+      }));
+
+    this.cards1 = this.generateLayout(this.level1Grid, 7, 3);
+    this.cards2 = this.generateLayout(this.level2Grid, 7, 3);
+    this.cards3 = this.generateLayout(this.level3Grid, 7, 3);
+    this.cards4 = this.generateLayout(this.level4Grid, 7, 3);
+    this.cards5 = this.generateLayout(this.level5Grid, 7, 3);
   }
 
   generateLayout(
